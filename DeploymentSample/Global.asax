cf05f77ddfae48e6a0716dc16b59f83af22ce7fb
Piero Viano <piero_viano@waters.com>
2019-04-16 15:08:42 +0300

    Add project files.

---
+++ DeploymentSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DeploymentSample.MvcApplication" Language="C#" %>
---
+++ DeploymentSample/PeopleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using DeploymentSample.Models;
+
+namespace DeploymentSample
+{
+    public class PeopleContext : DbContext  
+    {
+        public PeopleContext() : base("ConnectionString")
+        {
+
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
